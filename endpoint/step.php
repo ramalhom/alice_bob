898ccf57e32b69d97f2c19f19f070e1d4bceb1a2
ramalhomemf <70634347+ramalhomemf@users.noreply.github.com>
2022-01-17 00:02:16 +0100

    correction eve + trudi

--- endpoint/step.php
+++ endpoint/step.php
@@ -40,92 +40,6 @@ switch ($_POST['req']) {
                         </div>
 
 
-          </div>
-        </div>
-        <?php
-        } else if ($step==2) {
-        ?>
-        <div class="row">
-            <div class="col-md-12">
-                <h2>Méthode 2: cryptage symétrique</h2>
-                <img src="images/step2.png" class="img_step"/>
-                <p>Alice envoie un message à Bob sur Internet avec un cryptage symétrique.
-                    <button type="button" class="btn btn-link btn-bigger" data-toggle="modal" data-target="#step2Modal">
-                        <i class="fa fa-info-circle"></i>
-                    </button>
-                </p>
-            </div>
-        </div>
-        <?php
-        } else if ($step==3) {
-        ?>
-        <div class="row">
-            <div class="col-md-12">
-                <h2>Méthode 3: cryptage asymétrique</h2>
-                <img src="images/step3.png" class="img_step"/>
-                <p>Alice envoie un message à Bob sur Internet avec un cryptage asymétrique.
-                    <button type="button" class="btn btn-link btn-bigger" data-toggle="modal" data-target="#step3Modal">
-                        <i class="fa fa-info-circle"></i>
-                    </button>
-                </p>
-            </div>
-        </div>
-        <?php
-        }
-        ?>
-        <div class="row">
-        <?php
-            switch ($username) {
-                case 'Alice':
-                    $messageBob = $msg->get($session_id,$step,'Bob');
-                ?>
-            <div class="col-md-12">
-                <form class="form-border" onsubmit="return msg.save()">
-                    <div class="form-group">
-                        <input type="hidden" id="msg_session_id" value="<?=$session_id?>">
-                    </div>
-                    <div class="form-group">
-                        <input type="hidden" id="msg_username" value="<?=$username?>" />
-                    </div>
-                    <div class="form-group">
-                        <input type="hidden" id="msg_step" value="<?=$step?>" />
-                    </div>
-                        <?php if ($step>1) { ?>
-                    <div class="form-group">
-                        <label for="msg_private_key"><i class="fa fa-key"></i> Ma clé privée (nombre entier)</label>
-                        <input type="number" class="form-control" id="msg_private_key" value="<?=$message[1]?>" required>
-                    </div>
-                        <?php } else { ?>
-                    <div class="form-group">
-                        <input type="hidden" id="msg_private_key" value="" />
-                    </div>
-                        <?php } ?>
-                        <?php if ($step>2) { ?>
-                    <div class="form-group">
-                        <label for="msg_public_key"><i class="fa fa-key"></i> Clé publique de <?=$username?> (nombre entier)</label>
-                        <input type="number" class="form-control" id="msg_public_key" value="<?=$message[2]?>" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="msg_public_key_bob"><i class="fa fa-key"></i> Clé publique de Bob <a href="javascript:msg.list()"><i class="fas fa-sync"></i> Rafraîchir</a> </label>
-                        <?php if (isset($messageBob[2])) {?>
-                            <p class="message"><?=$messageBob[2]?></p>
-                        <?php } else {?>
-                            <p class="message"><i>Aucune clé définie</i></p>
-                        <?php } ?>
-                    </div>
-                        <?php } else { ?>
-                    <div class="form-group">
-                        <input type="hidden" id="msg_public_key" value="" />
-                    </div>
-                        <?php } ?>
-                    <?php if ($step>2) { ?>
-                    <div class="form-group">
-                        <label for="message"><i class="fa fa-lock"></i> Crypter avec ...</label>
-                        <select class="custom-select" id="msg_crypted_with" required>
-                            <option value="private_key_alice"<?php if ($message[3] == "private_key_alice") echo " selected"; ?>>Clé privé de Alice</option>
-                            <option value="public_key_alice"<?php if ($message[3] == "public_key_alice") echo " selected"; ?>>Clé publique de Alice</option>
-                            <option value="public_key_bob"<?php if ($message[3] == "public_key_bob") echo " selected"; ?>>Clé publique de Bob</option>
-                        </select>
                     </div>
                 </div>
             <?php
@@ -167,25 +81,40 @@ switch ($_POST['req']) {
                     case 'Alice':
                         $messageBob = $msg->get($session_id, $step, 'Bob');
                 ?>
-                    <?php if ($step > 1) { ?>
-                    <div class="col-md-12">
-                        <form class="form-border" onsubmit="return msg.save()">
-                            <div class="form-group">
-                                <input type="hidden" id="msg_session_id" value="<?=$session_id?>" />
-                                <input type="hidden" id="msg_username" value="<?=$username?>" />
-                                <input type="hidden" id="msg_step" value="<?=$step?>" />
-                                <input type="hidden" id="msg_message" value="" />
-                                <?php if ($step>1) { ?>
-                                    <label for="msg_private_key"><i class="fa fa-key"></i> Ma clé privée (nombre entier)</label>
-                                    <input type="number" class="form-control" id="msg_private_key" value="<?=$messageUser[1]?>" required>
+                        <div class="col-md-12">
+                            <form class="form-border" onsubmit="return msg.save()">
+                                <div class="form-group">
+                                    <input type="hidden" id="msg_session_id" value="<?= $session_id ?>">
+                                </div>
+                                <div class="form-group">
+                                    <input type="hidden" id="msg_username" value="<?= $username ?>" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="hidden" id="msg_step" value="<?= $step ?>" />
+                                </div>
+                                <?php if ($step > 1) { ?>
+                                    <div class="form-group">
+                                        <label for="msg_private_key"><i class="fa fa-key"></i> Clé privée (nombre entier)</label>
+                                        <input type="number" class="form-control" id="msg_private_key" value="<?= $message[1] ?>" required>
+                                    </div>
                                 <?php } else { ?>
                                     <div class="form-group">
                                         <input type="hidden" id="msg_private_key" value="" />
                                     </div>
                                 <?php } ?>
-                                <?php if ($step>2 and $username != 'Trudi' and $username != 'Eve') { ?>
-                                    <label for="msg_public_key"><i class="fa fa-key"></i> Ma clé publique (nombre entier)</label>
-                                    <input type="number" class="form-control" id="msg_public_key" value="<?=$messageUser[2]?>" required>
+                                <?php if ($step > 2) { ?>
+                                    <div class="form-group">
+                                        <label for="msg_public_key"><i class="fa fa-key"></i> Clé publique de <?= $username ?> (nombre entier)</label>
+                                        <input type="number" class="form-control" id="msg_public_key" value="<?= $message[2] ?>" required>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="msg_public_key_bob"><i class="fa fa-key"></i> Clé publique de Bob <a href="javascript:msg.list()"><i class="fas fa-sync"></i> Rafraîchir</a> </label>
+                                        <?php if (isset($messageBob[2])) { ?>
+                                            <p class="message"><?= $messageBob[2] ?></p>
+                                        <?php } else { ?>
+                                            <p class="message"><i>Aucune clé définie</i></p>
+                                        <?php } ?>
+                                    </div>
                                 <?php } else { ?>
                                     <div class="form-group">
                                         <input type="hidden" id="msg_public_key" value="" />
@@ -195,15 +124,9 @@ switch ($_POST['req']) {
                                     <div class="form-group">
                                         <label for="message"><i class="fa fa-lock"></i> Crypter avec ...</label>
                                         <select class="custom-select" id="msg_crypted_with" required>
-                                            <option value="public_key_alice"<?php if ($messageUser[3] == "public_key_alice") echo " selected"; ?>>Clé publique de Alice</option>
-                                            <option value="public_key_bob"<?php if ($messageUser[3] == "public_key_bob") echo " selected"; ?>>Clé publique de Bob</option>
-                                            <?php if ($username == 'Bob') { ?>
-                                                <option value="private_key_bob"<?php if ($messageUser[3] == "private_key_bob") echo " selected"; ?>>Clé privé de Bob</option>
-                                            <?php } else if ($username == 'Trudi') { ?>
-                                                <option value="private_key_trudi"<?php if ($messageUser[3] == "private_key_trudi") echo " selected"; ?>>Clé privé de Trudi</option>
-                                            <?php } else { ?>
-                                                <option value="private_key_eve"<?php if ($messageUser[3] == "private_key_eve") echo " selected"; ?>>Clé privé de Eve</option>
-                                             <?php } ?>
+                                            <option value="private_key_alice" <?php if ($message[3] == "private_key_alice") echo " selected"; ?>>Clé privé de Alice</option>
+                                            <option value="public_key_alice" <?php if ($message[3] == "public_key_alice") echo " selected"; ?>>Clé publique de Alice</option>
+                                            <option value="public_key_bob" <?php if ($message[3] == "public_key_bob") echo " selected"; ?>>Clé publique de Bob</option>
                                         </select>
                                     </div>
                                 <?php } else { ?>
@@ -251,13 +174,13 @@ switch ($_POST['req']) {
                                         <input type="hidden" id="msg_step" value="<?= $step ?>" />
                                         <input type="hidden" id="msg_message" value="" />
                                         <?php if ($step > 1) { ?>
-                                            <label for="msg_private_key"><i class="fa fa-key"></i> Clé privée (nombre entier)</label>
+                                            <label for="msg_private_key"><i class="fa fa-key"></i> Ma clé privée (nombre entier)</label>
                                             <input type="number" class="form-control" id="msg_private_key" value="<?= $messageUser[1] ?>" required>
                                         <?php } else { ?>
                                             <input type="hidden" id="msg_private_key" value="" />
                                         <?php } ?>
-                                        <?php if ($step > 2 and $username != 'Trudi') { ?>
-                                            <label for="msg_public_key"><i class="fa fa-key"></i> Clé publique (nombre entier)</label>
+                                        <?php if ($step > 2 and $username != 'Trudi' and $username != 'Eve') { ?>
+                                            <label for="msg_public_key"><i class="fa fa-key"></i> Ma clé publique (nombre entier)</label>
                                             <input type="number" class="form-control" id="msg_public_key" value="<?= $messageUser[2] ?>" required>
                                         <?php } else { ?>
                                             <input type="hidden" id="msg_public_key" value="" />
@@ -274,10 +197,12 @@ switch ($_POST['req']) {
                                                 <select class="custom-select" id="msg_crypted_with" required>
                                                     <option value="public_key_alice" <?php if ($messageUser[3] == "public_key_alice") echo " selected"; ?>>Clé publique de Alice</option>
                                                     <option value="public_key_bob" <?php if ($messageUser[3] == "public_key_bob") echo " selected"; ?>>Clé publique de Bob</option>
-                                                    <?php if ($username != 'Trudi') { ?>
+                                                    <?php if ($username == 'Bob') { ?>
                                                         <option value="private_key_bob" <?php if ($messageUser[3] == "private_key_bob") echo " selected"; ?>>Clé privé de Bob</option>
-                                                    <?php } else { ?>
+                                                    <?php } else if ($username == 'Trudi') { ?>
                                                         <option value="private_key_trudi" <?php if ($messageUser[3] == "private_key_trudi") echo " selected"; ?>>Clé privé de Trudi</option>
+                                                    <?php } else if ($username == 'Eve') { ?>
+                                                        <option value="private_key_eve" <?php if ($messageUser[3] == "private_key_eve") echo " selected"; ?>>Clé privé de Eve</option>
                                                     <?php } ?>
                                                 </select>
                                             </div>
