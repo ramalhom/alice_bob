4841b8ae0e2eb1c775f7e58de39795d4c20dc87d
Mario Ramalho <mario@ramalho.ch>
2022-04-02 00:21:30 +0200

    Update 3a-config.php

--- 3a-config.php
+++ 3a-config.php
@@ -5,10 +5,10 @@ error_reporting(E_ALL & ~E_NOTICE);
 // DATABASE SETTINGS
 // ! CHANGE TO YOUR OWN !
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'modulesemf_alicebob');
+define('DB_NAME', '');
 define('DB_CHARSET', 'utf8');
-define('DB_USER', 'modulesemf_alicebob');
-define('DB_PASSWORD', 'EmfModules114');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
 
 // AUTO PATH
 define('PATH_LIB', __DIR__ . DIRECTORY_SEPARATOR);
